0c24e4e923b70ec6268f234d7832c5c89cff7df9
Svetlana Burlakova <svetlya4ok83@gmail.com>
2023-03-09 13:43:43 +0400

    Main fucntion was added to the code

---
+++ Code/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+string [] CreateNewArray(string [] initArray)
+{
+    string newStringArray = string.Empty;
+    for (int i = 0; i < initArray.Length; i++)
+    {
+        if (initArray[i].Length<=3)
+        {
+            newStringArray += initArray[i] + ",";
+        }
+    }
+    string [] array = newStringArray.Split(',');
+    return array;
+}
+void PrintArray(string[] arr)
+{
+    for (int i =0; i< arr.Length;i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+string [] stringArray = {"qwerty","asdf","12",":)","Hi!"};
+string [] result = CreateNewArray(stringArray);
+PrintArray(result);
